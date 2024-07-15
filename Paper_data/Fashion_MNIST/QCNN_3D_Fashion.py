ce42ef788e81c2fdfc87685df66b840099979fc1
ptitbroussou <leo.monbroussou@gmail.com>
2024-07-15 16:39:37 +0200

    Cluster Run

--- Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
+++ Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
@@ -40,7 +40,7 @@ train_epochs = 10  # number of epoch we train
 test_interval = 1  # when the training epoch reaches an integer multiple of the test_interval, print the testing result
 criterion = torch.nn.CrossEntropyLoss()  # loss function
 output_scale = 20
-device = torch.device("mps")  # also torch.device("cpu"), or torch.device("mps") for macbook
+device = torch.device("cuda")  # also torch.device("cpu"), or torch.device("mps") for macbook
 
 # Here you can modify the RBS gate list that you want for the dense layer:
 # dense_full_gates is for the case qubit=O+J, dense_reduce_gates is for the case qubit=5.
