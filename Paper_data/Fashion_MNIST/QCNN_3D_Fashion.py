3900bd88cad646230c8d12a99ccaf44d3c516ba5
ptitbroussou <leo.monbroussou@gmail.com>
2024-07-15 16:33:46 +0200

    Cluster Run

--- Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
+++ Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
@@ -31,13 +31,13 @@ batch_size = 10  # batch number
 class_set = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # filter dataset
 kernel_layout = "all_connection" # you can use "pyramid" or "all_connection"
 medmnist_name = 'pathmnist'  # only useful when you use MedMNIST
-train_dataset_number = 10  # training dataset sample number
-test_dataset_number = 10  # testing dataset sample number
+train_dataset_number = int(1e4)  # training dataset sample number
+test_dataset_number = int(1e3)  # testing dataset sample number
 reduced_qubit = 5  # ATTENTION: please let binom(reduced_qubit,k) >= len(class_set)!
 is_shuffle = True  # shuffle for this dataset
 learning_rate = 1e-4  # step size for each learning steps
-train_epochs = 4  # number of epoch we train
-test_interval = 2  # when the training epoch reaches an integer multiple of the test_interval, print the testing result
+train_epochs = 10  # number of epoch we train
+test_interval = 1  # when the training epoch reaches an integer multiple of the test_interval, print the testing result
 criterion = torch.nn.CrossEntropyLoss()  # loss function
 output_scale = 20
 device = torch.device("mps")  # also torch.device("cpu"), or torch.device("mps") for macbook
