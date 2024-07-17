e0f574bd45fa1af6100f149df4df964b7656291c
ptitbroussou <leo.monbroussou@gmail.com>
2024-07-17 11:25:14 +0200

    Increasing Batch Size

--- Paper_data/Fashion_MNIST/QCNN_2D_Fashion.py
+++ Paper_data/Fashion_MNIST/QCNN_2D_Fashion.py
@@ -34,7 +34,7 @@ I = 32  # dimension of image we use. If you use 2 times conv and pool layers, pl
 O = I // 2  # dimension after pooling, usually you don't need to change this
 k = 2  # preserving subspace parameter, usually you don't need to change this
 #K = 4  # size of kernel in the convolution layer, please make it divisible by O=I/2
-batch_size = 1  # batch number
+batch_size = 30  # batch number
 class_set = [i for i in range(10)]  # filter dataset
 train_dataset_number = int(1e4)  # training dataset sample number
 test_dataset_number = int(1e3)  # testing dataset sample number
