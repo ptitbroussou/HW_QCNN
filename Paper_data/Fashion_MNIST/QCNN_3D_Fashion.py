fd275af90f260b0fb4e486795c9c933b117efc1a
ptitbroussou <leo.monbroussou@gmail.com>
2024-07-19 14:15:42 +0200

    exp

--- Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
+++ Paper_data/Fashion_MNIST/QCNN_3D_Fashion.py
@@ -40,12 +40,12 @@ batch_size = 12  # batch number
 class_set = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # filter dataset
 kernel_layout = "all_connection" # you can use "pyramid" or "all_connection"
 medmnist_name = 'pathmnist'  # only useful when you use MedMNIST
-train_dataset_number = int(1e4)  # training dataset sample number
-test_dataset_number = int(1e3)  # testing dataset sample number
+train_dataset_number = int(1e3)  # training dataset sample number
+test_dataset_number = int(1e2)  # testing dataset sample number
 reduced_qubit = 5  # ATTENTION: please let binom(reduced_qubit,k) >= len(class_set)!
 is_shuffle = True  # shuffle for this dataset
 learning_rate = 1e-3  # step size for each learning steps
-train_epochs = 30  # number of epoch we train
+train_epochs = 100  # number of epoch we train
 test_interval = 5  # when the training epoch reaches an integer multiple of the test_interval, print the testing result
 criterion = torch.nn.CrossEntropyLoss()  # loss function
 output_scale = 20
@@ -121,7 +121,7 @@ scheduler = ExponentialLR(optimizer, gamma=1)
 train_dataloader, test_dataloader = load_fashion_mnist(class_set, train_dataset_number, test_dataset_number, batch_size)
 network_state, training_loss_list, training_accuracy_list, testing_loss_list, testing_accuracy_list = train_globally(batch_size, I, J, network, train_dataloader, test_dataloader, optimizer, scheduler, criterion, output_scale, train_epochs, test_interval, stride, device)
 
-torch.save(network_state, "FashionMNIST_model")  # save network parameters
+torch.save(network_state, "FashionMNIST_100_epochs_model")  # save network parameters
 
 result_data = {'train_accuracy': training_accuracy_list,'train_loss': training_loss_list,'test_accuracy': testing_accuracy_list,'test_loss': testing_loss_list}
 #result_data['train_accuracy'] += training_accuracy_list
